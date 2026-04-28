ced6921072c82d5c9428b9f9df90b7d5c14e652c
A One <130599309+mraowens@users.noreply.github.com>
2026-04-28 15:12:12 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Student Name: Antawyone Owens
+    File Name: script.js
+    Date: 04/16/2026
+*/
+
+// Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
